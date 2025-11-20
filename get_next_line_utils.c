df3613e7b96e37567e49f827739715d6b7edd4f5
minyklc <suizumina@gmail.com>
2025-11-20 13:39:44 +0100

    wrong gnl

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: minpple <minpple@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 14:50:21 by minpple           #+#    #+#             */
-/*   Updated: 2025/11/20 12:40:38 by minpple          ###   ########.fr       */
+/*   Updated: 2025/11/20 13:39:37 by minpple          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,27 @@
 
 void	ft_putstr(char *s)
 {
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
 }
 
 char	*ft_newmalloc(char *before, int nb)
 {
+	char	*new;
+	
+	new = malloc(sizeof(char) * nb);
+	nb--;
+	while (nb >= 0)
+	{
+		new[nb] = before[nb];
+		nb--;
+	}
+	free(before);
+	return (new);
 }
